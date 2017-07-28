8a4476f572af659025d59706e7ec2ec9c6cf8d98
vitkuzmenko <kuzmenko.v.u@gmail.com>
2017-07-28 08:51:10 +0300

    build

--- PayCardsRecognizerExample/CompleteViewController.swift
+++ PayCardsRecognizerExample/CompleteViewController.swift
@@ -15,21 +15,10 @@ class CompleteViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func openSDK(sender: AnyObject) {
+        let url = URL(string: "https://github.com/faceterteam/PayCardsRecognizer_iOS")!
+        UIApplication.shared.openURL(url)
     }
-    */
 
 }
